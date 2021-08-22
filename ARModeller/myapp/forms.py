f3bf57d04de62037481a4cba1fae2c99089c189b
Ishita Goyal <goyalishita0301@gmail.com>
2021-08-23 01:49:41 +0530

    Ishita's commit2

--- ARModeller/myapp/forms.py
+++ ARModeller/myapp/forms.py
@@ -1,7 +1,7 @@
 from django.forms import ModelForm
 from .models import *
 
-class PizzaForm(ModelForm):
+class InputForm(ModelForm):
     class Meta:
         model=RecievingFromApp
         fields="__all__"
